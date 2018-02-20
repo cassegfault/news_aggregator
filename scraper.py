a8794ee2b8853132d4eb5211e0dfbe1da7dc0a1e
Chris Pauley <chrispauley92@gmail.com>
2018-02-19 20:31:34 -0600

    timezone

--- scraper.py
+++ scraper.py
@@ -85,7 +85,7 @@ def parse_reddit_source(data, source):
 	return response_list
 
 def parse_hn_timestamp(ts):
-	return time.mktime(datetime.datetime.strptime(ts, '%a, %d %b %Y %H:%M:%S').timetuple())
+	return time.mktime(datetime.datetime.strptime(ts, '%a, %d %b %Y %H:%M:%S +0000').timetuple())
 
 def parse_hacker_news(data, source):
 	tree = ET.fromstring(data)
